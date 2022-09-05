c76224c4793caa5c66978d83fa518d526dcaf1f8
ProsenjitB-Xpeed <prosenjit@xpeedstudio.com>
2022-09-06 01:38:38 +0600

    deployable

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -10,7 +10,7 @@ router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
 router.use(authController.restrictTo('admin', 'lead-guide'));
 
 router.route('/')
-.get(bookingController.getallBookings)
+.get(bookingController.getallBooking)
 .post(bookingController.createBooking);
 
 router.route('/:id')
